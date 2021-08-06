78cfae661a5842862c1660d7cb377bcc6f3191d4
wills blake <richwblake@gmail.com>
2021-08-05 19:21:17 -0700

    Added greeting in console

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Game from './game.js';
 
+console.log('%c Welcome to my site!', 'color: lightblue;');
+console.log('I use this page to show off newly created ideas. Nothing too special, but something fun that might have a positive impact on someone :)');
+
 let canvas = document.getElementById('gameScreen');
 let context = canvas.getContext('2d');
 
